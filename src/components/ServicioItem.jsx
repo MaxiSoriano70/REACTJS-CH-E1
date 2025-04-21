ccc844b942ba0f322a9bb14896a1adf08d2f6a36
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-04-21 03:29:39 -0300

    Servicios y ServicioItem

--- src/components/ServicioItem.jsx
+++ src/components/ServicioItem.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import styles from '../css/servicios.module.css';
 
-const ServicioItem = () => {
+const ServicioItem = ({ image, title, description }) => {
+    const imageUrl = new URL(`../assets/img/${image}`, import.meta.url).href;
+
     return (
-        <div>
-        
+        <div className={styles.cardServicio}>
+        <img src={imageUrl} alt={title} />
+        <h5>{title}</h5>
+        <p>{description}</p>
         </div>
-    )
-}
+    );
+};
 
 export default ServicioItem;
